926523e3c12edeb90ba786a01f876f6836fabfd7
Ivan Vaccari <ivan.vaccari91@gmail.com>
2022-01-10 17:32:17 +0100

    Updates

---
+++ precache-manifest.4aba445f7aa670181431d3620cb78565.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0eb1b7e6ba21985389add7a344bee4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f044ab703379c1d66ad1",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "efac12a8932ef2dba949",
+    "url": "/static/js/2.acfc954e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.acfc954e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f044ab703379c1d66ad1",
+    "url": "/static/js/main.8c396f4d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "bf5d63e53c85a48678c1c5aa68cf2899",
+    "url": "/static/media/CNR.bf5d63e5.png"
+  },
+  {
+    "revision": "90edb32c42288573c2ed6128ab18aa2c",
+    "url": "/static/media/Unige.90edb32c.png"
+  },
+  {
+    "revision": "d2a8a5def0271cf3b6d13494a2830d2c",
+    "url": "/static/media/ivanvaccari.d2a8a5de.png"
+  },
+  {
+    "revision": "b948c977876298960aa067afef515654",
+    "url": "/static/media/resume.b948c977.pdf"
+  }
+]);
